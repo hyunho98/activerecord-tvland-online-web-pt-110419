9f41ebad4528ad3f5b22578e8e5ecd2d1b65c3e3
Alex Lee <hyunho98@gmail.com>
2020-02-16 23:30:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,4 +5,8 @@ class Show < ActiveRecord::Base
   def actors_list
     self.actors.collect{|actor| "#{actor.full_name}"}
   end
+
+  def build_network(attributes)
+    Network.create(attributes)
+  end
 end
